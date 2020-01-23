d522d268417c614504d2b5bca0989d8167ae6a19
Simone <simonelega.sl@gmail.com>
2020-01-23 18:15:22 +0100

    all'inserimento, il messaggio compare nella finestra con formattazione

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+$('document').ready(function(){
 
+  $('#insert_someth').keypress(
+      function (event) {
+        if(event.which == 13 || event.keyCode == 13) {
+          var text = $('#insert_someth').val();
+          var elementNew = $('.template p').clone();
+          elementNew.append(text);
+          $('.window_body').append(elementNew);
+        }
+      }
+    );
+
+});
