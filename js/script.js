97ee27d65c2b73a1ec01afa853b2e642b72359d2
Simone <simonelega.sl@gmail.com>
2020-01-27 20:38:55 +0100

    ordine js

--- js/script.js
+++ js/script.js
@@ -2,35 +2,19 @@ $('document').ready(function(){
 
 // funzione che al click inserisce un messaggio nella conversazione. Nella callback ricevo un messaggio di ritorno
   $('.clicca').click(function(){
-    var text = $('#insert_someth').val();
-    if(text != 0){
-      var elementNew = $('.template .message').clone();
-      $('.window_body.active').append(elementNew);
-      elementNew.addClass('sent');
-      elementNew.find('.text p').append(text);
-    } setTimeout(function(){
+    message(), setTimeout(function(){
       ciao();
     }, 1000);
   }
 );
-// funzione di callback per il messaggio di ritorno
-  function ciao(){
-    var textReceive = 'ciao';
-    var elementNew = $('.template .message').clone();
-    $('.window_body.active').append(elementNew);
-    elementNew.addClass('receive');
-    elementNew.find('.text p').append(textReceive);
-  }
+
 // funzione ricerca tra i nomi dei contatti
   $('.search input').keyup(function(){
-    var search = $('.search-name').val().toLowerCase();
-    var name = $('.name p:first-child').text().toLowerCase();
-    var container = $("p.username:contains(" + search + ")");
-    $('.discussion_profile').addClass('active_name_off');
-    container.parents('.discussion_profile').removeClass('active_name_off');
+    search();
   });
 
-  $('.discussion_profile').click(function(){;
+  // funzione per passare da una discussione all'altra senza perdere i contenuti
+  $('.discussion_profile').click(function(){
     var discussionNumber = $(this).attr('data-contact');
     $('.window_body').each(function(){
       if($(this).attr('data-contact')==discussionNumber){
@@ -38,9 +22,7 @@ $('document').ready(function(){
         $(this).addClass('active');
       }
     })
-
   });
-
 
   $(document).on('click','.hidden_elements', function(){
     $(this).parent().siblings('.hidden_menu').show();
@@ -48,9 +30,34 @@ $('document').ready(function(){
 
   $(document).on('click','.delete', function(){
     $(this).parent().parent().remove();
-
   });
 
-
-
 });
+
+// funzione messaggio
+  function message(){
+    var text = $('#insert_someth').val();
+    if(text != 0){
+      var elementNew = $('.template .message').clone();
+      $('.window_body.active').append(elementNew);
+      elementNew.addClass('sent');
+      elementNew.find('.text p').append(text);
+    };
+  };
+// funzione di callback per il messaggio di ritorno
+  function ciao(){
+    var textReceive = 'ciao';
+    var elementNew = $('.template .message').clone();
+    $('.window_body.active').append(elementNew);
+    elementNew.addClass('receive');
+    elementNew.find('.text p').append(textReceive);
+  };
+
+
+  function search(){
+    var search = $('.search-name').val().toLowerCase();
+    var name = $('.name p:first-child').text().toLowerCase();
+    var container = $("p.username:contains(" + search + ")");
+    $('.discussion_profile').addClass('active_name_off');
+    container.parents('.discussion_profile').removeClass('active_name_off');
+  }
